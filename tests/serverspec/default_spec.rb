61d1f43055bc39e953cedb32807a56333b133214
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-10-12 16:53:35 +0900

    test if packages not in the official repo can be installed
    also, remove `yum repolist all` test because it takes time and the test
    below cover the same thing.

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -3,23 +3,36 @@ require 'spec_helper'
 packages = %w[ epel-release ]
 repo_name = 'epel'
 
+# choose a package that is not included in the default repos
+package_not_in_default_repo = %w[ python-pip ]
+
 packages.each do |p|
   describe package(p) do
     it { should be_installed }
   end
 end
 
-# installed?
-describe command('yum repolist all') do
-  its(:stdout) { should match(/^#{ repo_name }/) }
-end
-
+## installed?
+#describe command('yum repolist all') do
+#  its(:stdout) { should match(/^#{ repo_name }/) }
+#  its(:exit_status) { should eq 0 }
+#end
+#
 # enabled?
 describe command('yum repolist enabled') do
   its(:stdout) { should match(/^#{ repo_name }/) }
+  its(:exit_status) { should eq 0 }
 end
 
 describe file("/etc/yum.repos.d/#{ repo_name }.repo") do
   it { should be_file }
   its(:content) { should match(/^\[#{ repo_name }\]\nenabled = 1\ngpgcheck = 1\nmirrorlist = #{ Regexp.escape('http://mirrors.fedoraproject.org/mirrorlist?repo=epel-7&arch=x86_64') }/) }
+end
+
+package_not_in_default_repo.each do |p|
+  describe command("yum install -y #{p}") do
+    its(:stdout) { should match(/Complete!/) }
+    its(:stderr) { should_not match(/Error:/) }
+    its(:exit_status) { should eq 0 }
+  end
 end
