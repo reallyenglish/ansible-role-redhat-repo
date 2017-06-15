f202552c03bab6d52da601560d764d6e5a3c3d11
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-15 17:01:56 +0700

    [bugfix] QA (#9)
    * QA
    
    * QA

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,10 +1,10 @@
-require 'spec_helper'
+require "spec_helper"
 
-packages = %w[ epel-release ]
-repo_name = 'epel'
+packages = %w(epel-release)
+repo_name = "epel"
 
 # choose a package that is not included in the default repos
-package_not_in_default_repo = %w[ python-pip ]
+package_not_in_default_repo = %w(python-pip)
 
 packages.each do |p|
   describe package(p) do
@@ -12,15 +12,24 @@ packages.each do |p|
   end
 end
 
-describe file("/etc/yum.repos.d/#{ repo_name }.repo") do
+describe file("/etc/yum.repos.d/#{repo_name}.repo") do
   it { should be_file }
   its(:content) { should match(/^\[#{ repo_name }\]\nenabled = 1\ngpgcheck = 1\nmirrorlist = #{ Regexp.escape('http://mirrors.fedoraproject.org/mirrorlist?repo=epel-7&arch=x86_64') }/) }
 end
 
 package_not_in_default_repo.each do |p|
   describe command("yum install -y #{p}") do
-    its(:stdout) {      skip 'yum sometimes takes more than 600 sec https://github.com/reallyenglish/ansible-role-redhat-repo/issues/4' if ENV['JENKINS_HOME']; should match(/Complete!/) }
-    its(:stderr) {      skip 'yum sometimes takes more than 600 sec https://github.com/reallyenglish/ansible-role-redhat-repo/issues/4' if ENV['JENKINS_HOME']; should_not match(/Error:/) }
-    its(:exit_status) { skip 'yum sometimes takes more than 600 sec https://github.com/reallyenglish/ansible-role-redhat-repo/issues/4' if ENV['JENKINS_HOME']; should eq 0 }
+    its(:stdout) do
+      skip "yum sometimes takes more than 600 sec https://github.com/reallyenglish/ansible-role-redhat-repo/issues/4" if ENV["JENKINS_HOME"]
+      should match(/Complete!/)
+    end
+    its(:stderr) do
+      skip "yum sometimes takes more than 600 sec https://github.com/reallyenglish/ansible-role-redhat-repo/issues/4" if ENV["JENKINS_HOME"]
+      should_not match(/Error:/)
+    end
+    its(:exit_status) do
+      skip "yum sometimes takes more than 600 sec https://github.com/reallyenglish/ansible-role-redhat-repo/issues/4" if ENV["JENKINS_HOME"]
+      should eq 0
+    end
   end
 end
